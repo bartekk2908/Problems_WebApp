0a4f2b50a8ffc8908c4021244599d4b2a9fb427f
Bartosz Krzysztoszek <bartekk2908@op.pl>
2023-08-08 04:24:14 +0200

    models

---
+++ webapp/db_queries.py
@@ -0,0 +1,24 @@
+import manage
+manage.main()
+
+from problems_app.models import Problems
+from django.utils import timezone
+
+
+def reset_table():
+    Problems.objects.all().delete()
+
+
+if __name__ == "__main__":
+    print("\n\n")
+
+    # reset_table()
+
+    Problems(id=1, problem_content_text="Elo", solution_content_text="Tak",
+             pub_date=timezone.now()).save()
+
+    print(Problems.objects.all())
+
+    print(Problems.objects.values_list('pk', flat=True))
+
+    reset_table()
--- webapp/problems_app/models.py
+++ webapp/problems_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Problems(models.Model):
+    problem_content_text = models.CharField(max_length=200)
+    solution_content_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return f"Problem: {self.problem_content_text}, " \
+               f"Solution: {self.solution_content_text}, " \
+               f"Publish Date: {self.pub_date}"
